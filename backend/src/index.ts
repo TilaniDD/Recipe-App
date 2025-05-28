0ab4972fd5cc2e387a9e2ae8ebb52c4cacec1193
TilaniDD <tilanidharmarathne@gmail.com>
2025-05-28 23:30:08 +0530

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
-import express from 'express'
-import cors from 'cors'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import * as RecipeAPI from './recipe-api';
 
 const app=express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/api/recipes/search",async(req,res)=>{
-    res.json({message: 'success!'})
-})
+app.get("/api/recipes/search", async (req: Request, res: Response)=>{
+    const searchTerm=req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+    const results = RecipeAPI.searchRecipes(searchTerm,page);
+
+    return res.json(results);
+});
 
 app.listen(5000,()=>{
     console.log("server running on localhost:5000");
-})
+});
