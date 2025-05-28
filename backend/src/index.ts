c355e7c9cf42295e5da8ba2089a605d6c83bff95
Tilani Dharmarathne <tilanidharmarathne2000@gmail.com>
2025-05-28 23:32:56 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/recipes/search", async (req: Request, res: Response)=>{
+app.get("/api/recipes/search", async (req, res)=>{
     const searchTerm=req.query.searchTerm as string;
     const page = parseInt(req.query.page as string);
     const results = RecipeAPI.searchRecipes(searchTerm,page);
